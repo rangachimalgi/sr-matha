8990efe22d79d141b7db4f2e8a4654f8fea27417
ranga chimalgi <rangachimalgi123@gmail.com>
2023-10-09 19:14:39 +0530

    update orderRoutes and env

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import Order from "../models/Orders.js";
 import multer from "multer";
-import { uploadReport, downloadReports, sendReportsByEmail } from "../controllers/OrderController.js";
+import { uploadReport, downloadReports, sendReportsByEmail } from "../controllers/orderController.js";
 
 const router = express.Router();
 
